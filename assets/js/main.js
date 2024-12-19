bf934c02f3e75bafaf8807a9d112f83157f51c84
Mariano Paul Ferreyra <maferreyra@sofredigital.com.ar>
2024-12-19 17:29:51 -0300

    final commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -30,19 +30,6 @@
         headerToggle();
       }
     });
-
-  });
-
-  /**
-   * Toggle mobile nav dropdowns
-   */
-  document.querySelectorAll('.navmenu .toggle-dropdown').forEach(navmenu => {
-    navmenu.addEventListener('click', function(e) {
-      e.preventDefault();
-      this.parentNode.classList.toggle('active');
-      this.parentNode.nextElementSibling.classList.toggle('dropdown-active');
-      e.stopImmediatePropagation();
-    });
   });
 
   /**
